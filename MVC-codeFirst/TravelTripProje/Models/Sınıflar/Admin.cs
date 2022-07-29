fa29f76a5c2c505468651a094623d0f27d83eeb7
hafizenalbant <61289808+hafizenalbant@users.noreply.github.com>
2022-07-29 16:51:21 +0300

    SeyahatBlog-Mvc-Codefirst

---
+++ MVC-codeFirst/TravelTripProje/Models/Sınıflar/Admin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TravelTripProje.Models.Sınıflar
+{
+    public class Admin
+    {
+        [Key]
+        public int ID { get; set; }
+        public String Kullanici { get; set; }
+        public String Sifre { get; set; }
+    }
+}
---
+++ MVC-codeFirst/TravelTripProje/Models/Sınıflar/AnaSayfa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TravelTripProje.Models.Sınıflar
+{
+    public class AnaSayfa
+    {
+        [Key]
+        public int ID { get; set; }
+        public int Baslik { get; set; }
+        public int Aciklama { get; set; }
+
+    }
+}
